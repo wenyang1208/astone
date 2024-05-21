394be43296e68ad5eccdbc6dfb0d0019f6156495
Ivan <wlim0061@student.monash.edu>
2024-05-22 00:56:52 +0800

    Adjust login page to have proper navigation features.

--- Project/frontend/src/pages/login.js
+++ Project/frontend/src/pages/login.js
@@ -1,56 +1,70 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Box, TextField, Button, Typography } from '@mui/material';
+import { Container, Box, TextField, Button, Typography, CssBaseline, Grid } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Header2 from '../components/header2'; // Adjust the import path if necessary
+
+const defaultTheme = createTheme();
 
 const Login = () => {
   return (
-    <Container maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <Box component="form" sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-          <Typography variant="body2" color="textSecondary" align="center" sx={{ mt: 2 }}>
-            Don't have an account? <Link to="/signup" style={{ textDecoration: 'none', color: 'inherit' }}>Sign Up</Link>
-          </Typography>
-        </Box>
-      </Box>
-    </Container>
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header2 />
+        <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '80vh' }}>
+          <Grid item xs={12} sm={8} md={5}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Typography component="h1" variant="h5">
+                Login
+              </Typography>
+              <Box component="form" sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Typography variant="body2" color="textSecondary" align="center" sx={{ mb: 1 }}>
+                  Don't have an account? <Link to="/signup" style={{ textDecoration: 'none', color: 'blue' }}>Sign Up</Link>
+                </Typography>
+                <Typography variant="body2" color="textSecondary" align="center">
+                  <Link to="/forgotpassword" style={{ textDecoration: 'none', color: 'gray' }}>Forgot Password?</Link>
+                </Typography>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 };
 
