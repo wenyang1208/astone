8d4a5a424b3494d678b8e9e9a092bc12964439b0
RuiEnKoe <rkoe0003@student.monash.edu>
2024-05-22 14:21:32 +0800

    add search bar in men, unisex, and women page

--- Project/frontend/src/components/searchBar.js
+++ Project/frontend/src/components/searchBar.js
@@ -1,34 +1,52 @@
 import React from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
-import Typography, { Input, Toolbar } from '@mui/material';
+import Typography, { IconButton, Input, Toolbar } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-// import backgroundImage from '../assests/mask-group-2.png';
+import backgroundImage from '../assets/mask-group-2.png';
 
 // search bar style
-// const SearchStyleBar = styled('div')(({ theme }) => ({
+const SearchBarWrapper = styled('div')(({ theme }) => ({
     
-//     backgroundImage: `url(${backgroundImage})`,
-//     backgroundSize: 'cover',
-//     backgroundRepeat: 'no-repeat',
-//     backgroundPosition: 'center',
+    // position: 'relative',
+    borderRadius: '25px',  // Set high borderRadius for oval shape
+    // set height of search bar
+    height: '60px',
+    width: '70%',
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(5, 0, 0, 5),
+      marginRight: theme.spacing(2),
+      width: '80%',
+    },
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    // center background img within the search bar
+    backgroundPosition: 'center',
 
-// }));
+}));
 
 // main search bar style
 const Search = styled('div')(({ theme }) => ({
+
     position: 'relative',
-    borderRadius: theme.shape.borderRadius,
+    borderRadius: '50px',  // Set high borderRadius for oval shape
+    // search bar colour
     backgroundColor: alpha(theme.palette.common.white, 0.15),
     '&:hover': {
       backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
+    // set height of search bar
+    // height: '50px',
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: '100%',
     [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(3),
-      width: 'auto',
+      width: '100%',
     },
   }));
 
@@ -42,10 +60,9 @@ const Search = styled('div')(({ theme }) => ({
       transition: theme.transitions.create('width'),
       width: '100%',
       [theme.breakpoints.up('md')]: {
-        width: '20ch',
+       },
       },
-    },
-  }));
+    }));
 
 // search icon wrapper
 const SearchIconWrapper = styled('div')(({ theme }) => ({
@@ -60,26 +77,28 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
 
 
 // simple search bar
+// need to change in the further to make it more dyamic
 function SearchBar () {
 
     return (
-        // <SearchStyleBar
+
         <Toolbar>
             <Search>
 
             <SearchIconWrapper>
-                <SearchIcon />   
+                <SearchIcon /> 
+                <IconButton/>
             </SearchIconWrapper>
 
             <StyledInputBase
               placeholder="Search by name.…"
+              // arial-label: accessible label for screen readers
               inputProps={{ 'arial-label': 'search'}}
                
             />
 
         </Search>
         </Toolbar>
-        // </SearchStyleBar>
 
         );
         
