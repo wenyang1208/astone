13d7ce055b76d577b4c8536c4a1e5efddc21922d
Tay Ming Hui <mtay0031@student.monash.edu>
2024-05-22 13:20:13 +0800

    added styles

--- Project/frontend/src/pages/product/productList.js
+++ Project/frontend/src/pages/product/productList.js
@@ -25,35 +25,58 @@ function ProductList() {
     }, []);
   
     return (
-      <div style={{ marginLeft: '20px', marginTop: '10px' }}>
-        <header style={{ fontSize: '30px' }}>Shop name</header>
-        <hr />
-        <h1>Products</h1>
-        <div style={{ display: 'flex', justifyContent: 'flex-end', width: '95%' }}>
-          <Box sx={{ marginBottom: '20px' }}>
+      <div style={{backgroundColor: '#eedafe', paddingTop: '20px'}}>
+        <div sx= {{position: 'fixed', top: 0, left: 0, width: '100%', zIndex: 1000}}>
+        <header style={{fontSize: '30px', paddingLeft: '20px'}}>Shop name</header>
+        <hr/>
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'space-between'}}>
+          <p style={{ display: 'flex', flex: '1', paddingLeft: '20px', fontSize: '30px', fontWeight: 'bold'}}>Products</p>
+          <Box sx={{display: 'flex', alignItems: 'end', margin: '0', padding: '0'}}>
             <Link to="/product/create" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" sx={{ backgroundColor: '#A020F0', '&:hover': { backgroundColor: '#7D0DC3' }, color: 'white', marginRight: '0.5rem' }}>
-                Add
+              <Button 
+                variant="contained"
+                sx={{ 
+                  backgroundColor: '#A020F0', 
+                  '&:hover': { backgroundColor: '#7D0DC3' }, 
+                  color: 'white', 
+                  marginRight: '0.5rem' , 
+                  borderRadius: '40px', // Make the button circular
+                  height: '40px', // Adjust height as needed
+                  width: '40px', // Adjust width as needed
+                  padding: 0, // Remove padding to ensure circular shape
+                  fontSize: '25px'
+                }}
+              >
+                +
               </Button>
             </Link>
           </Box>
-          <Box sx={{ marginBottom: '20px' }}>
-            <Link to="/product/create" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" sx={{ backgroundColor: '#A020F0', '&:hover': { backgroundColor: '#7D0DC3' }, color: 'white' }}>
-                Remove
-              </Button>
-            </Link>
-          </Box>
-          </div>
-          {products.map((output, id) => (
-            <div key={id}>
-              <Typography variant="h6">{`Product name: ${output.name}`}</Typography>
-              <Typography variant="h6">{`Product price: ${output.currency} ${output.price}`}</Typography>
-              <Typography variant="h6">{`Product quantity: ${output.stock}`}</Typography>
-              <br />
-            </div>
-          ))}
+        </div>
+        </div>
         
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '35px', paddingLeft: '20px', paddingTop: '20px'}}>
+          {products.map((output, id) => (
+              <Box 
+                  key={id} 
+                  sx={{ 
+                      padding: '30px', 
+                      borderRadius: '8px',
+                      boxShadow: '0 4px 7px #babcbb', 
+                      minWidth: '280px',
+                      maxWidth: '350px',
+                      backgroundColor: '#f9f9f9',
+                      marginBottom: '10px',
+                  }}
+              >
+                  <Typography variant="h6" sx={{fontSize: '14px'}}>
+                          {`Product ID: ast234567890`}<br />
+                          {`Name: ${output.name}`}<br />
+                          {`Price: ${output.currency} ${output.price}`}<br />
+                          {`Stock: ${output.stock}`}
+                  </Typography>
+              </Box>
+          ))}
+        </div> 
       </div>
     );
   }
