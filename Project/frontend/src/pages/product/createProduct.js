a160540905676d594142bde45340b81575c1d36b
Tay Ming Hui <mtay0031@student.monash.edu>
2024-05-23 08:47:36 +0800

    fix

--- Project/frontend/src/pages/product/createProduct.js
+++ Project/frontend/src/pages/product/createProduct.js
@@ -4,18 +4,24 @@ import { useFormik } from 'formik';
 import { ChromePicker } from 'react-color';
 import tinycolor from 'tinycolor2';
 import Select from 'react-select';
+import image from './/image.png';
 import {
   Box,
   TextField,
   Button,
   FormControl,
   Typography,
-  InputAdornment,
+  InputAdornment, 
+  IconButton,
+  Popover,
 } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
 import { ProductService } from '../../services/ProductService';
 
 function CreateProduct() {
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [popoverOpen, setPopoverOpen] = React.useState(false);
   const productService = new ProductService();
 
   const formik = useFormik({
@@ -65,10 +71,10 @@ function CreateProduct() {
     { value: 'men', label: 'Men' },
     { value: 'women', label: 'Women' },
     { value: 'kids', label: 'Kids' },
-    // { value: 'top', label: 'Top' },
-    // { value: 'bottom', label: 'Bottom' },
-    // { value: 'accessories', label: 'Accessories' },
-    // { value: 'footwear', label: 'Footwear' },
+    { value: 'top', label: 'Top' },
+    { value: 'bottom', label: 'Bottom' },
+    { value: 'accessories', label: 'Accessories' },
+    { value: 'footwear', label: 'Footwear' },
   ];
 
   const sizeOptions = [
@@ -95,13 +101,22 @@ function CreateProduct() {
     const selected = selectedOptions.map(option => ({ value: option.value, label: option.label }));
     formik.setFieldValue('sizes', selected);
   };
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+    setPopoverOpen(true);
+  };
+  
+  const handlePopoverClose = () => {
+    setPopoverOpen(false);
+  };
   
   const customSelectStyles = {
     control: (provided) => ({
       ...provided,
       minHeight: '50px',
       borderRadius: '4px',
-      borderColor: '#ccc',
+      borderColor: '#b7b7b7',
       '&:hover': {
         borderColor: '#aaa',
       },
@@ -128,72 +143,106 @@ function CreateProduct() {
   };
 
   return (
-    <Box sx={{ margin: '20px' }}>
-      <Typography variant="h4">Create Product</Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
-        <Box
-          sx={{
-            flex: 1,
-            border: '1px solid #ccc',
-            padding: '20px',
-            textAlign: 'center',
-            marginRight: '20px',
-          }}
-        >
-          <Typography variant="h6">Add Image</Typography>
-          {/* Image upload component goes here */}
-        </Box>
-        <Box component="form" onSubmit={formik.handleSubmit} sx={{ flex: 1 }}>
+    <div style={{backgroundColor: '#f2e2fe', minHeight: '100vh', padding: '20px'}}>
+      <Box sx={{ margin: '20px' }}>
+        <Typography variant="h4" sx={{paddingBottom: '3px'}}>Create Your Product</Typography>
+        <Typography sx={{fontSize: '15px'}} >Start your fashion journey with Astone and unlock endless possibilities for creating captivating, trend-setting products.</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+          <Box
+            sx={{
+              flex: 1,
+              border: '2px solid #b7b7b7',
+              borderRadius: '4px',
+              padding: '20px',
+              textAlign: 'center',
+              marginRight: '20px',
+              marginTop: '15px',
+            }}
+          >
+            <Typography variant="h6">Add Image</Typography>
+          
+          </Box>
+          <Box component="form" onSubmit={formik.handleSubmit} sx={{ flex: 1 }}>
           <FormControl fullWidth margin="normal">
-            <TextField
-              id="name"
-              name="name"
-              label="Name"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.name}
-              error={formik.touched.name && Boolean(formik.errors.name)}
-              helperText={formik.touched.name && formik.errors.name}
-            />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <TextField
-              id="description"
-              name="description"
-              label="Description"
-              type="text"
-              onChange={formik.handleChange}
-              value={formik.values.description}
-              error={formik.touched.description && Boolean(formik.errors.description)}
-              helperText={formik.touched.description && formik.errors.description}
-            />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <Typography>Category</Typography>
-            <Select
-              isMulti
-              name="categories"
-              options={categoryOptions}
-              className="basic-multi-select"
-              classNamePrefix="select"
-              onChange={handleCategoryChange}
-              value={formik.values.category}
-              styles={customSelectStyles}
-            />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <Typography>Color</Typography>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <ChromePicker
-              id="description"
-              name="description"
-              color={formik.values.colors}
-              onChange={color => formik.setFieldValue('color', [{name: tinycolor(color).toName(), code: color.hex}])}
-            />
-            </Box>
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <Typography>Sizes</Typography>
+              <TextField
+                id="name"
+                name="name"
+                label="Name"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                error={formik.touched.name && Boolean(formik.errors.name)}
+                helperText={formik.touched.name && formik.errors.name}
+                style={{ backgroundColor: 'white' }}
+              />
+            </FormControl>
+            <FormControl fullWidth margin="normal">
+              <TextField
+                id="description"
+                name="description"
+                label="Description"
+                type="text"
+                onChange={formik.handleChange}
+                value={formik.values.description}
+                error={formik.touched.description && Boolean(formik.errors.description)}
+                helperText={formik.touched.description && formik.errors.description}
+                style={{ backgroundColor: 'white' }}
+              />
+            </FormControl>
+            <FormControl fullWidth margin="normal">
+              <Typography>Category</Typography>
+              <Select
+                isMulti
+                name="categories"
+                options={categoryOptions}
+                className="basic-multi-select"
+                classNamePrefix="select"
+                onChange={handleCategoryChange}
+                value={formik.values.category}
+                styles={customSelectStyles}
+              />
+            </FormControl>
+            <FormControl fullWidth margin="normal">
+              <Typography>Color</Typography>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <ChromePicker
+                id="description"
+                name="description"
+                color={formik.values.colors}
+                onChange={color => formik.setFieldValue('color', [{name: tinycolor(color).toName(), code: color.hex}])}
+              />
+              </Box>
+            </FormControl>
+            <FormControl fullWidth margin="normal">
+            <Typography sx={{ display: 'flex', alignItems: 'center' }}>Sizes
+              <IconButton 
+                size="small"
+                onClick={handlePopoverOpen}
+                sx={{ marginLeft: 1 }}
+              >
+                <InfoIcon />
+              </IconButton>
+            </Typography>
+            <Popover
+              open={popoverOpen}
+              anchorEl={anchorEl}
+              onClose={handlePopoverClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+            >
+              <Box sx={{ padding: 2 }}>
+                {/* Add your size measurement guide content here */}
+                <Typography>Size Measurement Guide:</Typography>
+                <img src={image} alt="Size Chart" style={{ maxWidth: '85%' }} />
+                <Typography sx={{fontSize: '12px'}}>Astone supports true and clear size measurement, ensuring every product matches its detailed size guide. </Typography>
+              </Box>
+            </Popover>
             <Select
               isMulti
               name="sizes"
@@ -205,56 +254,59 @@ function CreateProduct() {
               styles={customSelectStyles}
             />
           </FormControl>
-          
-          <FormControl fullWidth margin="normal">
-            <TextField
-              id="price"
-              name="price"
-              label="Price"
-              type="number"
-              onChange={formik.handleChange}
-              value={formik.values.price}
-              error={formik.touched.price && Boolean(formik.errors.price)}
-              helperText={formik.touched.price && formik.errors.price}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    MYR
-                    <Box
-                      component="span"
-                      sx={{
-                        margin: '0 10px',
-                        borderLeft: '1px solid #ccc',
-                        height: '24px',
-                      }}
-                    />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <TextField
-              id="stock"
-              name="stock"
-              label="Stock"
-              type="number"
-              onChange={formik.handleChange}
-              value={formik.values.stock}
-              error={formik.touched.stock && Boolean(formik.errors.stock)}
-              helperText={formik.touched.stock && formik.errors.stock}
-            />
-          </FormControl>
-          <Button
-            type="submit"
-            disabled={formik.isSubmitting}
-            sx={{ backgroundColor: '#007bff', color: 'white', '&:hover': { backgroundColor: '#0056b3' }, marginTop: '20px' }}
-          >
-            Submit
-          </Button>
+            
+            <FormControl fullWidth margin="normal">
+              <TextField
+                id="price"
+                name="price"
+                label="Price"
+                type="number"
+                onChange={formik.handleChange}
+                value={formik.values.price}
+                error={formik.touched.price && Boolean(formik.errors.price)}
+                helperText={formik.touched.price && formik.errors.price}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      MYR
+                      <Box
+                        component="span"
+                        sx={{
+                          margin: '0 10px',
+                          borderLeft: '1px solid #b7b7b7',
+                          height: '24px',
+                        }}
+                      />
+                    </InputAdornment>
+                  ),
+                }}
+                style={{ backgroundColor: 'white' }}
+              />
+            </FormControl>
+            <FormControl fullWidth margin="normal">
+              <TextField
+                id="stock"
+                name="stock"
+                label="Stock"
+                type="number"
+                onChange={formik.handleChange}
+                value={formik.values.stock}
+                error={formik.touched.stock && Boolean(formik.errors.stock)}
+                helperText={formik.touched.stock && formik.errors.stock}
+                style={{ backgroundColor: 'white' }}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              disabled={formik.isSubmitting}
+              sx={{ backgroundColor: '#b357ff', color: 'white', '&:hover': { backgroundColor: '#0056b3' }, marginTop: '20px' , width:'80px', height:'40px'}}
+            >
+              Submit
+            </Button>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </div>
   );
 }
 
