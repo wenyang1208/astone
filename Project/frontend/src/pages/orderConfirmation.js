ab4c3ed0bb8078bf4d10df5c79a85dc72b54afca
RuiEnKoe <rkoe0003@student.monash.edu>
2024-10-01 22:06:52 +0800

    re-update the initial code in orderConfirmation.js file

--- Project/frontend/src/pages/orderConfirmation.js
+++ Project/frontend/src/pages/orderConfirmation.js
@@ -9,8 +9,20 @@ function OrderConfirmationPage({ orderDetails }) {
       <Typography variant="h4" gutterBottom>
         Order Confirmation
       </Typography>
-
-
+      <Box>
+        <Typography variant="h6" gutterBottom>
+          Order Details
+        </Typography>
+        <Typography variant="body1">
+          Order ID: {orderDetails.id}
+        </Typography>
+        <Typography variant="body1">
+          Total Price: {'MYR' + orderDetails.total_price}
+        </Typography>
+        <Typography variant="body1">
+          Address: {orderDetails.address}
+        </Typography>
+        {/* Add more order details as needed */}
       </Box>
     </Container>
   );
