44b22fa0619f7d7128ec6ee2ed9fd8ad1579bc7a
RuiEnKoe <rkoe0003@student.monash.edu>
2024-10-01 21:57:41 +0800

    Update about us.js file and checkout.js file to move code from orderconfirmation.js to simulate return/cancel order

--- Project/frontend/src/pages/about.js
+++ Project/frontend/src/pages/about.js
@@ -1,113 +1,59 @@
 import React from 'react';
-import {Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography} from '@mui/material';
-import { useState } from 'react';
-import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import MyAppBar2 from '../components/appBar2'; // Importing MyAppBar2
+import Footer2 from '../components/footer2'; // Importing Footer2
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from "@mui/material/CssBaseline";
+import {Box, Container, Typography} from '@mui/material';
+
+const defaultTheme = createTheme();
 
 const About = () => {
 
-  // for return order
-  const [returnInitiated, setReturnInitiated] = useState(false); 
-  const [openReturnDialog, setOpenReturnDialog] = useState(false); 
-  // for cancel order
-  const [cancelInitiated, setCancelInitiated] = useState(false);  
-  const [openCancelDialog, setOpenCancelDialog] = useState(false); 
-
-  // Function to handle opening the return confirmation dialog
-  const handleReturnClick = () => {
-    setOpenReturnDialog(true);
-  };
-
-  // Function to handle opening the cancel confirmation dialog
-  const handleCancelClick = () => {
-    setOpenCancelDialog(true);
-  };
-
-  // Function to handle confirming the return
-  const confirmReturn = () => {
-    setReturnInitiated(true);
-    setOpenReturnDialog(false);
-  };
-
-    // Function to handle confirming the cancel
-    const confirmCancel = () => {
-      setCancelInitiated(true);
-      setOpenCancelDialog(false);
-      cancelNotification();
-    };
-
-  // Function to handle closing the dialog without returning
-  const handleCloseDialog = () => {
-    setReturnInitiated(false);
-    setOpenReturnDialog(false);
-  };
-
-  // Function to show cancel is successful performed
-  const cancelNotification = () => {
-    toast.success('Your order has been succesfully cancelled!', 
-    {
-        position:"top-center",
-        autoClose: 3000
-    });
-  };
-
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the about page.</p>
+ 
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
 
-        {/* Add a button to simulate return confirmation*/}
-        {!returnInitiated && (
-        <Button variant="contained" color="primary" onClick={handleReturnClick} sx={{mt: 2 }}>
-          Return Product
-        </Button>
-        )}
-
-        {/* Add a button to simulate cancel confirmation*/}
-        {!cancelInitiated && !returnInitiated && (
-          <Button variant="contained" color="primary" onClick={handleCancelClick} sx={{mt: 2, ml: 2 }}>
-            Cancel Order
-          </Button>
-        )}
-
-        {/* Dialog for Return Confirmation */}
-        <Dialog open={openReturnDialog} onClose={handleCloseDialog}>
-        <DialogTitle> Confirm Return</DialogTitle>
-        <DialogContent>
-          <Typography variant = 'body1' gutterBottom>
-            Are you sure you want to return this product?
+      <MyAppBar2 />
+  
+      <Container style={{ textAlign: 'center', padding: '70px 0'}}>
+        <Box component="header"
+          sx={{
+            py: { xs: 5, sm: 8, md: 3 }, // responsive padding
+            px: 2,
+            mt: "auto",
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[250]
+                : theme.palette.grey[800],
+          }}>
+          <Typography variant="h4" gutterBottom>
+          Astone
           </Typography>
-            <Typography variant="h7" gutterBottom>
-            Refund Details
+          <Typography variant="body1" paragraph>
+            Astone is your ultimate destination for fashion, offering a diverse range of styles for everyone.
+            From casual wear to elegant outfits, we ensure that you find the perfect fit for every occasion.
           </Typography>
-          <Typography variant="body2" >
-            Order ID: 123445
+          <Typography variant="h6" gutterBottom>
+            Why Choose Astone?
           </Typography>
-          <Typography variant="body2">
-            Refund Amount: 34.5
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog} color="primary">Cancel</Button>
-          <Button onClick={confirmReturn} color="primary">Confirm Return</Button>
-        </DialogActions>
-      </Dialog>
+          <ul style={{ listStyle: 'none', padding: 0 }}>
+            <li>
+              <Typography variant="body1">✨ Variety: Explore countless choices tailored to all tastes and styles.</Typography>
+            </li>
+            <li>
+              <Typography variant="body1">💎 Quality: We source only the finest materials to keep you looking great and feeling comfortable.</Typography>
+            </li>
+            <li>
+              <Typography variant="body1">💰 Affordability: Trendy fashion doesn't have to break the bank. Enjoy stylish options at unbeatable prices!</Typography>
+            </li>
+          </ul>
+        </Box>
+      </Container>
 
-        {/* Dialog for Cancel Confirmation */}
-        <Dialog open={openCancelDialog} onClose={handleCloseDialog}>
-        <DialogTitle>Confirm Cancelation</DialogTitle>
-        <DialogContent>
-          <Typography>Are you sure you want to cancel this order?</Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog} color="primary">Back</Button>
-          <Button onClick={confirmCancel} color="primary">Confirm Cancel</Button>
-        </DialogActions>
-      </Dialog>
-
-      <ToastContainer />
-
-    </div>
+      <Footer2 />
+    </ThemeProvider>
   );
 };
 
--- Project/frontend/src/pages/orderConfirmation.js
+++ Project/frontend/src/pages/orderConfirmation.js
@@ -1,55 +1,8 @@
 import React from 'react';
 import { Container, Typography, Box, Button, Dialog, DialogActions, DialogContent, DialogTitle} from '@mui/material';
 import { useState } from 'react';
-import {ToastContainer, toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 function OrderConfirmationPage({ orderDetails }) {
-
-  // for return order
-  const [returnInitiated, setReturnInitiated] = useState(false); 
-  const [openReturnDialog, setOpenReturnDialog] = useState(false); 
-  // for cancel order
-  const [cancelInitiated, setCancelInitiated] = useState(false);  
-  const [openCancelDialog, setOpenCancelDialog] = useState(false); 
-
-  // Function to handle opening the return confirmation dialog
-  const handleReturnClick = () => {
-    setOpenReturnDialog(true);
-  };
-
-  // Function to handle opening the cancel confirmation dialog
-  const handleCancelClick = () => {
-    setOpenCancelDialog(true);
-  };
-
-  // Function to handle confirming the return
-  const confirmReturn = () => {
-    setReturnInitiated(true);
-    setOpenReturnDialog(false);
-  };
-
-    // Function to handle confirming the cancel
-    const confirmCancel = () => {
-      setCancelInitiated(true);
-      setOpenCancelDialog(false);
-      cancelNotification();
-    };
-
-  // Function to handle closing the dialog without returning
-  const handleCloseDialog = () => {
-    setReturnInitiated(false);
-    setOpenReturnDialog(false);
-  };
-
-  // Function to show cancel is successful performed
-  const cancelNotification = () => {
-    toast.success('Your order has been succesfully cancelled!', 
-    {
-        position:"top-center",
-        autoClose: 3000
-    });
-  };
 
   return (
     <Container>
@@ -71,57 +24,6 @@ function OrderConfirmationPage({ orderDetails }) {
         </Typography>
         {/* Add more order details as needed */}
 
-
-        {/* Add a button to simulate return confirmation*/}
-        {!returnInitiated && (
-        <Button variant="contained" color="primary" onClick={handleReturnClick} sx={{mt: 2 }}>
-          Return Product
-        </Button>
-        )}
-
-        {/* Add a button to simulate cancel confirmation*/}
-        {!cancelInitiated && !returnInitiated && (
-          <Button variant="contained" color="primary" onClick={handleCancelClick} sx={{mt: 2, ml: 2 }}>
-            Cancel Order
-          </Button>
-        )}
-
-        {/* Dialog for Return Confirmation */}
-        <Dialog open={openReturnDialog} onClose={handleCloseDialog}>
-        <DialogTitle> Confirm Return</DialogTitle>
-        <DialogContent>
-          <Typography variant = 'body1' gutterBottom>
-            Are you sure you want to return this product?
-          </Typography>
-            <Typography variant="h7" gutterBottom>
-            Refund Details
-          </Typography>
-          <Typography variant="body2" >
-            Order ID: 123445
-          </Typography>
-          <Typography variant="body2">
-            Refund Amount: 34.5
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog} color="primary">Cancel</Button>
-          <Button onClick={confirmReturn} color="primary">Confirm Return</Button>
-        </DialogActions>
-      </Dialog>
-
-        {/* Dialog for Cancel Confirmation */}
-        <Dialog open={openCancelDialog} onClose={handleCloseDialog}>
-        <DialogTitle>Confirm Cancelation</DialogTitle>
-        <DialogContent>
-          <Typography>Are you sure you want to cancel this order?</Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseDialog} color="primary">Back</Button>
-          <Button onClick={confirmCancel} color="primary">Confirm Cancel</Button>
-        </DialogActions>
-      </Dialog>
-
-      <ToastContainer />
 
       </Box>
     </Container>
